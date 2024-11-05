5c6fc86d3853b747d57dd8898161b0389abf0fb6
d.kosenko <dimweb.tech@mail.ru>
2024-11-05 21:49:22 +0300

    add simple screenshot2video script

---
+++ comfyui_sandbox/reshade_screenshot_recorder.py
@@ -0,0 +1,40 @@
+import pyautogui
+from pynput import keyboard
+import time
+controller = keyboard.Controller()
+
+is_started = False
+
+def press_screen():
+    controller.press(keyboard.Key.print_screen)
+    time.sleep(0.05)
+    controller.release(keyboard.Key.print_screen)
+    
+    global is_started
+    if is_started:
+        press_screen()
+    
+
+def on_press(key):
+    # try:
+    #     print("alphanumeric key {0} pressed".format(key.char))
+    # except AttributeError:
+    #     print("special key {0} pressed".format(key))
+    if key == keyboard.Key.f8:
+        global is_started 
+        is_started = True
+        press_screen()
+
+
+def on_release(key):
+    print("{0} released".format(key))
+    if key == keyboard.Key.f9:
+        # Stop listener
+        global is_started
+        is_started = False
+        print("Stop listener")
+        return False
+
+
+with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
