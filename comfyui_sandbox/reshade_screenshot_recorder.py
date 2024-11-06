7c7117be4428ea8f207e953a0ccd43da451c1177
dmitry kosenko <dimweb.tech@mail.ru>
2024-11-06 00:57:05 +0000

    update readme

--- comfyui_sandbox/reshade_screenshot_recorder.py
+++ comfyui_sandbox/reshade_screenshot_recorder.py
@@ -1,19 +1,19 @@
 import pyautogui
 from pynput import keyboard
 import time
+
 controller = keyboard.Controller()
 
 is_started = False
 
+
 def press_screen():
-    controller.press(keyboard.Key.print_screen)
-    time.sleep(0.05)
-    controller.release(keyboard.Key.print_screen)
-    
     global is_started
-    if is_started:
-        press_screen()
-    
+    while is_started:
+        controller.press(keyboard.Key.print_screen)
+        time.sleep(0.1)
+        controller.release(keyboard.Key.print_screen)
+
 
 def on_press(key):
     # try:
@@ -21,7 +21,7 @@ def on_press(key):
     # except AttributeError:
     #     print("special key {0} pressed".format(key))
     if key == keyboard.Key.f8:
-        global is_started 
+        global is_started
         is_started = True
         press_screen()
 
