c96664dd771bc2adef3b9a0877dbc2cf91fe5094
dmitry kosenko <dimweb.tech@mail.ru>
2025-01-01 19:17:17 +0000

    update to 1024

--- diffusers/examples/instruct_pix2pix/run_sd_pix2pix.sh
+++ diffusers/examples/instruct_pix2pix/run_sd_pix2pix.sh
@@ -7,10 +7,10 @@ accelerate launch --mixed_precision="bf16" train_instruct_pix2pix.py \
     --pretrained_model_name_or_path=$MODEL_NAME \
     --dataset_name=$DATASET_ID \
     --enable_xformers_memory_efficient_attention \
-    --resolution=256 --random_flip \
-    --train_batch_size=8 --gradient_accumulation_steps=4 \
+    --resolution=1024 --random_flip \
+    --train_batch_size=2 --gradient_accumulation_steps=8 \
     --max_train_steps=1500 \
-    --checkpointing_steps=5000 --checkpoints_total_limit=1 \
+    --checkpointing_steps=500 --checkpoints_total_limit=1 \
     --learning_rate=5e-05 --max_grad_norm=1 --lr_warmup_steps=0 \
     --conditioning_dropout_prob=0.05 \
     --mixed_precision=bf16 \
