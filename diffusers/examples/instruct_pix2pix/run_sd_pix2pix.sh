83994aeaa7d83805bff092afe2eb153af12bd82c
dmitry kosenko <dimweb.tech@mail.ru>
2025-01-02 01:37:43 +0000

    add autofolder generation

--- diffusers/examples/instruct_pix2pix/run_sd_pix2pix.sh
+++ diffusers/examples/instruct_pix2pix/run_sd_pix2pix.sh
@@ -1,7 +1,12 @@
-MODEL_NAME="stable-diffusion-v1-5/stable-diffusion-v1-5"
+# MODEL_NAME="stable-diffusion-v1-5/stable-diffusion-v1-5"
+MODEL_NAME="stablediffusionapi/juggernaut-reborn"
 # export DATASET_ID="fusing/instructpix2pix-1000-samples"
 DATASET_ID="dim/nfs_pix2pix_1920_1080_v5"
-output_dir="nfs_pix2pix"
+mkdir -p models
+timestamp=$(date +%s)
+folder_name="nfs_pix2pix_$timestamp"
+WANDB_NAME=$folder_name
+output_dir="models/$folder_name"
 
 accelerate launch --mixed_precision="bf16" train_instruct_pix2pix.py \
     --pretrained_model_name_or_path=$MODEL_NAME \
