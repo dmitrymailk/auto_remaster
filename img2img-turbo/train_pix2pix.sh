8f6a5ba11418f0fd827a637e9aaa1dbbb879d853
_dimweb_ <dimweb.tech@mail.ru>
2025-01-11 14:23:07 +0000

    add inference update train

--- img2img-turbo/train_pix2pix.sh
+++ img2img-turbo/train_pix2pix.sh
@@ -6,13 +6,15 @@ output_dir="output/pix2pix_turbo/$folder_name"
 accelerate launch src/train_pix2pix_turbo.py \
     --pretrained_model_name_or_path="stabilityai/sd-turbo" \
     --output_dir=$output_dir \
-    --dataset_folder="dim/nfs_pix2pix_1920_1080_v5" \
+    --dataset_folder="dim/nfs_pix2pix_1920_1080_v6" \
     --train_batch_size=2 \
-    --enable_xformers_memory_efficient_attention --viz_freq 25 \
+    --enable_xformers_memory_efficient_attention --viz_freq 100 \
     --track_val_fid \
     --train_image_prep="resized_crop_512" \
     --test_image_prep="resized_crop_512" \
     --lora_rank_unet=128 \
+    --eval_freq=500 \
+    --max_train_steps=100000 \
     --report_to "wandb" \
     # --report_to "" 
     # --mixed_precision="bf16" \
