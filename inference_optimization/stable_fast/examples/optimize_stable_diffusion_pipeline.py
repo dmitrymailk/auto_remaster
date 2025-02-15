0d2eabf40bf4cdaa25e2772f3eaaf5fe4c0336e6
d.kosenko <dimweb.tech@mail.ru>
2025-02-15 00:25:57 +0000

    add repo

--- inference_optimization/stable_fast/examples/optimize_stable_diffusion_pipeline.py
+++ inference_optimization/stable_fast/examples/optimize_stable_diffusion_pipeline.py
@@ -1,21 +1,28 @@
-MODEL = 'runwayml/stable-diffusion-v1-5'
+# MODEL = "runwayml/stable-diffusion-v1-5"
+MODEL = "stabilityai/sd-turbo"
+# MODEL = "segmind/tiny-sd"
 VARIANT = None
 CUSTOM_PIPELINE = None
-SCHEDULER = 'EulerAncestralDiscreteScheduler'
+SCHEDULER = "EulerAncestralDiscreteScheduler"
 LORA = None
 CONTROLNET = None
-STEPS = 30
-PROMPT = 'best quality, realistic, unreal engine, 4K, a beautiful girl'
+STEPS = 1
+# PROMPT = "best quality, realistic, unreal engine, 4K, a beautiful cat"
+PROMPT = "best quality, realistic, unreal engine, 4K, a beautiful girl"
 NEGATIVE_PROMPT = None
 SEED = None
 WARMUPS = 3
 BATCH = 1
-HEIGHT = None
-WIDTH = None
+# HEIGHT = 512
+# WIDTH = 512
+HEIGHT = 480
+WIDTH = 640
 INPUT_IMAGE = None
 CONTROL_IMAGE = None
-OUTPUT_IMAGE = None
+OUTPUT_IMAGE = "/code/inference_optimization/stable_fast/test.jpg"
 EXTRA_CALL_KWARGS = None
+# TINY_VAE = False
+TINY_VAE = True
 
 import importlib
 import inspect
@@ -23,73 +30,76 @@ import argparse
 import time
 import json
 import torch
-from PIL import (Image, ImageDraw)
+from PIL import Image, ImageDraw
 from diffusers.utils import load_image
-from sfast.compilers.diffusion_pipeline_compiler import (compile,
-                                                         CompilationConfig)
+from sfast.compilers.diffusion_pipeline_compiler import compile, CompilationConfig
+from diffusers import EulerAncestralDiscreteScheduler, LCMScheduler, AutoencoderTiny
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model', type=str, default=MODEL)
-    parser.add_argument('--variant', type=str, default=VARIANT)
-    parser.add_argument('--custom-pipeline', type=str, default=CUSTOM_PIPELINE)
-    parser.add_argument('--scheduler', type=str, default=SCHEDULER)
-    parser.add_argument('--lora', type=str, default=LORA)
-    parser.add_argument('--controlnet', type=str, default=CONTROLNET)
-    parser.add_argument('--steps', type=int, default=STEPS)
-    parser.add_argument('--prompt', type=str, default=PROMPT)
-    parser.add_argument('--negative-prompt', type=str, default=NEGATIVE_PROMPT)
-    parser.add_argument('--seed', type=int, default=SEED)
-    parser.add_argument('--warmups', type=int, default=WARMUPS)
-    parser.add_argument('--batch', type=int, default=BATCH)
-    parser.add_argument('--height', type=int, default=HEIGHT)
-    parser.add_argument('--width', type=int, default=WIDTH)
-    parser.add_argument('--extra-call-kwargs',
-                        type=str,
-                        default=EXTRA_CALL_KWARGS)
-    parser.add_argument('--input-image', type=str, default=INPUT_IMAGE)
-    parser.add_argument('--control-image', type=str, default=CONTROL_IMAGE)
-    parser.add_argument('--output-image', type=str, default=OUTPUT_IMAGE)
+    parser.add_argument("--model", type=str, default=MODEL)
+    parser.add_argument("--variant", type=str, default=VARIANT)
+    parser.add_argument("--custom-pipeline", type=str, default=CUSTOM_PIPELINE)
+    parser.add_argument("--scheduler", type=str, default=SCHEDULER)
+    parser.add_argument("--lora", type=str, default=LORA)
+    parser.add_argument("--controlnet", type=str, default=CONTROLNET)
+    parser.add_argument("--steps", type=int, default=STEPS)
+    parser.add_argument("--prompt", type=str, default=PROMPT)
+    parser.add_argument("--negative-prompt", type=str, default=NEGATIVE_PROMPT)
+    parser.add_argument("--seed", type=int, default=SEED)
+    parser.add_argument("--warmups", type=int, default=WARMUPS)
+    parser.add_argument("--batch", type=int, default=BATCH)
+    parser.add_argument("--height", type=int, default=HEIGHT)
+    parser.add_argument("--width", type=int, default=WIDTH)
+    parser.add_argument("--extra-call-kwargs", type=str, default=EXTRA_CALL_KWARGS)
+    parser.add_argument("--input-image", type=str, default=INPUT_IMAGE)
+    parser.add_argument("--control-image", type=str, default=CONTROL_IMAGE)
+    parser.add_argument("--output-image", type=str, default=OUTPUT_IMAGE)
+    parser.add_argument("--tiny-vae", type=str, default=TINY_VAE)
     parser.add_argument(
-        '--compiler',
+        "--compiler",
         type=str,
-        default='sfast',
-        choices=['none', 'sfast', 'compile', 'compile-max-autotune'])
-    parser.add_argument('--quantize', action='store_true')
-    parser.add_argument('--no-fusion', action='store_true')
+        default="sfast",
+        choices=["none", "sfast", "compile", "compile-max-autotune"],
+    )
+    parser.add_argument("--quantize", action="store_true")
+    parser.add_argument("--no-fusion", action="store_true")
     return parser.parse_args()
 
 
-def load_model(pipeline_cls,
-               model,
-               variant=None,
-               custom_pipeline=None,
-               scheduler=None,
-               lora=None,
-               controlnet=None):
+def load_model(
+    pipeline_cls,
+    model,
+    variant=None,
+    custom_pipeline=None,
+    scheduler=None,
+    lora=None,
+    controlnet=None,
+):
     extra_kwargs = {}
     if custom_pipeline is not None:
-        extra_kwargs['custom_pipeline'] = custom_pipeline
+        extra_kwargs["custom_pipeline"] = custom_pipeline
     if variant is not None:
-        extra_kwargs['variant'] = variant
+        extra_kwargs["variant"] = variant
     if controlnet is not None:
         from diffusers import ControlNetModel
-        controlnet = ControlNetModel.from_pretrained(controlnet,
-                                                     torch_dtype=torch.float16)
-        extra_kwargs['controlnet'] = controlnet
-    model = pipeline_cls.from_pretrained(model,
-                                         torch_dtype=torch.float16,
-                                         **extra_kwargs)
+
+        controlnet = ControlNetModel.from_pretrained(
+            controlnet, torch_dtype=torch.float16
+        )
+        extra_kwargs["controlnet"] = controlnet
+    model = pipeline_cls.from_pretrained(
+        model, torch_dtype=torch.float16, **extra_kwargs
+    )
     if scheduler is not None:
-        scheduler_cls = getattr(importlib.import_module('diffusers'),
-                                scheduler)
+        scheduler_cls = getattr(importlib.import_module("diffusers"), scheduler)
         model.scheduler = scheduler_cls.from_config(model.scheduler.config)
     if lora is not None:
         model.load_lora_weights(lora)
         model.fuse_lora()
     model.safety_checker = None
-    model.to(torch.device('cuda'))
+    model.to(torch.device("cuda"))
     return model
 
 
@@ -100,17 +110,19 @@ def compile_model(model):
     # It might be slow for Triton to generate, compile and fine-tune kernels.
     try:
         import xformers
+
         config.enable_xformers = True
     except ImportError:
-        print('xformers not installed, skip')
+        print("xformers not installed, skip")
     # NOTE:
     # When GPU VRAM is insufficient or the architecture is too old, Triton might be slow.
     # Disable Triton if you encounter this problem.
     try:
         import triton
+
         config.enable_triton = True
     except ImportError:
-        print('Triton not installed, skip')
+        print("Triton not installed, skip")
     # NOTE:
     # CUDA Graph is suggested for small batch sizes and small resolutions to reduce CPU overhead.
     # My implementation can handle dynamic shape with increased need for GPU memory.
@@ -167,6 +179,14 @@ def main():
         lora=args.lora,
         controlnet=args.controlnet,
     )
+    if args.tiny_vae:
+        model.vae
+        model.vae = AutoencoderTiny.from_pretrained(
+            "madebyollin/taesd",
+            torch_device="cuda",
+            torch_dtype=torch.float16,
+        )
+        model.vae = model.vae.cuda()
 
     height = args.height or model.unet.config.sample_size * model.vae_scale_factor
     width = args.width or model.unet.config.sample_size * model.vae_scale_factor
@@ -175,53 +195,53 @@ def main():
 
         def quantize_unet(m):
             from diffusers.utils import USE_PEFT_BACKEND
+
             assert USE_PEFT_BACKEND
-            m = torch.quantization.quantize_dynamic(m, {torch.nn.Linear},
-                                                    dtype=torch.qint8,
-                                                    inplace=True)
+            m = torch.quantization.quantize_dynamic(
+                m, {torch.nn.Linear}, dtype=torch.qint8, inplace=True
+            )
             return m
 
         model.unet = quantize_unet(model.unet)
-        if hasattr(model, 'controlnet'):
+        if hasattr(model, "controlnet"):
             model.controlnet = quantize_unet(model.controlnet)
 
     if args.no_fusion:
-        torch.jit.set_fusion_strategy([('STATIC', 0), ('DYNAMIC', 0)])
+        torch.jit.set_fusion_strategy([("STATIC", 0), ("DYNAMIC", 0)])
 
-    if args.compiler == 'none':
+    if args.compiler == "none":
         pass
-    elif args.compiler == 'sfast':
+    elif args.compiler == "sfast":
         model = compile_model(model)
-    elif args.compiler in ('compile', 'compile-max-autotune'):
-        mode = 'max-autotune' if args.compiler == 'compile-max-autotune' else None
+    elif args.compiler in ("compile", "compile-max-autotune"):
+        mode = "max-autotune" if args.compiler == "compile-max-autotune" else None
         model.unet = torch.compile(model.unet, mode=mode)
-        if hasattr(model, 'controlnet'):
+        if hasattr(model, "controlnet"):
             model.controlnet = torch.compile(model.controlnet, mode=mode)
         model.vae = torch.compile(model.vae, mode=mode)
     else:
-        raise ValueError(f'Unknown compiler: {args.compiler}')
+        raise ValueError(f"Unknown compiler: {args.compiler}")
 
     if args.input_image is None:
         input_image = None
     else:
         input_image = load_image(args.input_image)
-        input_image = input_image.resize((width, height),
-                                         Image.LANCZOS)
+        input_image = input_image.resize((width, height), Image.LANCZOS)
 
     if args.control_image is None:
         if args.controlnet is None:
             control_image = None
         else:
-            control_image = Image.new('RGB', (width, height))
+            control_image = Image.new("RGB", (width, height))
             draw = ImageDraw.Draw(control_image)
-            draw.ellipse((width // 4, height // 4,
-                          width // 4 * 3, height // 4 * 3),
-                         fill=(255, 255, 255))
+            draw.ellipse(
+                (width // 4, height // 4, width // 4 * 3, height // 4 * 3),
+                fill=(255, 255, 255),
+            )
             del draw
     else:
         control_image = load_image(args.control_image)
-        control_image = control_image.resize((width, height),
-                                             Image.LANCZOS)
+        control_image = control_image.resize((width, height), Image.LANCZOS)
 
     def get_kwarg_inputs():
         kwarg_inputs = dict(
@@ -231,36 +251,41 @@ def main():
             width=width,
             num_inference_steps=args.steps,
             num_images_per_prompt=args.batch,
-            generator=None if args.seed is None else torch.Generator(
-                device='cuda').manual_seed(args.seed),
-            **(dict() if args.extra_call_kwargs is None else json.loads(
-                args.extra_call_kwargs)),
+            generator=(
+                None
+                if args.seed is None
+                else torch.Generator(device="cuda").manual_seed(args.seed)
+            ),
+            **(
+                dict()
+                if args.extra_call_kwargs is None
+                else json.loads(args.extra_call_kwargs)
+            ),
         )
         if input_image is not None:
-            kwarg_inputs['image'] = input_image
+            kwarg_inputs["image"] = input_image
         if control_image is not None:
             if input_image is None:
-                kwarg_inputs['image'] = control_image
+                kwarg_inputs["image"] = control_image
             else:
-                kwarg_inputs['control_image'] = control_image
+                kwarg_inputs["control_image"] = control_image
         return kwarg_inputs
 
     # NOTE: Warm it up.
     # The initial calls will trigger compilation and might be very slow.
     # After that, it should be very fast.
     if args.warmups > 0:
-        print('Begin warmup')
+        print("Begin warmup")
         for _ in range(args.warmups):
             model(**get_kwarg_inputs())
-        print('End warmup')
+        print("End warmup")
 
     # Let's see it!
     # Note: Progress bar might work incorrectly due to the async nature of CUDA.
     kwarg_inputs = get_kwarg_inputs()
     iter_profiler = IterationProfiler()
-    if 'callback_on_step_end' in inspect.signature(model).parameters:
-        kwarg_inputs[
-            'callback_on_step_end'] = iter_profiler.callback_on_step_end
+    if "callback_on_step_end" in inspect.signature(model).parameters:
+        kwarg_inputs["callback_on_step_end"] = iter_profiler.callback_on_step_end
     begin = time.time()
     output_images = model(**kwarg_inputs).images
     end = time.time()
@@ -271,16 +296,16 @@ def main():
     for image in output_images:
         print_image(image, max_width=80)
 
-    print(f'Inference time: {end - begin:.3f}s')
+    print(f"Inference time: {end - begin:.3f}s")
     iter_per_sec = iter_profiler.get_iter_per_sec()
     if iter_per_sec is not None:
-        print(f'Iterations per second: {iter_per_sec:.3f}')
+        print(f"Iterations per second: {iter_per_sec:.3f}")
     peak_mem = torch.cuda.max_memory_allocated()
-    print(f'Peak memory: {peak_mem / 1024**3:.3f}GiB')
+    print(f"Peak memory: {peak_mem / 1024**3:.3f}GiB")
 
     if args.output_image is not None:
         output_images[0].save(args.output_image)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
